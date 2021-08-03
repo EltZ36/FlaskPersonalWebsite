017b120c9633e23aafb6d5a6c93805bde32bd91f
Elton <32944003+EltZ36@users.noreply.github.com>
2021-08-02 19:46:12 -0700

    Update views.py

--- project/views.py
+++ project/views.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+  
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+  
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
